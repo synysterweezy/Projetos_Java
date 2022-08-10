51988882a99be1020286b4a061040754dec250c5
Claver <edward.claver@icomp.ufam.edu.br>
2022-08-09 22:52:25 -0400

    lab 8 completo e certo!

--- LAB8/app/src/main/java/com/example/titansensorlocal/MainActivity.java
+++ LAB8/app/src/main/java/com/example/titansensorlocal/MainActivity.java
@@ -1,26 +1,135 @@
 package com.example.titansensorlocal;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
     private ToggleButton toggleButton;
     private Button updateLocation;
+    private SensorManager sensorManager;
+    private boolean ledStatus;
+    private CameraManager mCameraManager;
+    private String mCameraId;
     TextView latitudeTextView, longitTextView;
+
+    public boolean getLedStatus() {
+        return ledStatus;
+    }
+
+    public void setLedStatus(boolean ledStatus) {
+        this.ledStatus = ledStatus;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         latitudeTextView = findViewById(R.id.latTextView);
         longitTextView = findViewById(R.id.lonTextView);
         updateLocation = findViewById(R.id.updateLocation);
         toggleButton = findViewById(R.id.onOffFlashlight);
 
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(listener, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+        boolean isFlashAvailable;
+        isFlashAvailable = getApplicationContext().getPackageManager()
+                .hasSystemFeature(PackageManager.FEATURE_CAMERA_FRONT);
+        if (!isFlashAvailable) {
+            showNoFlashError();
+        }
+        this.setLedStatus(false);
+
+        mCameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            mCameraId = mCameraManager.getCameraIdList()[1];
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                switchFlashLight(isChecked);
+            }
+        });
+
+
 
     }
+    private SensorEventListener listener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            float xValue = Math.abs(event.values[0]);
+            float yValue = Math.abs(event.values[1]);
+            float zValue = Math.abs(event.values[2]);
+            if (xValue > 10 || yValue > 10 || zValue > 10) {
+                Toast.makeText(MainActivity.this, "shake function activated", Toast.LENGTH_SHORT).show();
+                changeLedStatus();
+                Log.d("SENSOR", "Balancou...");
+
+            }
+        }
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+    };
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (sensorManager != null) {
+            sensorManager.unregisterListener(listener);
+        }
+    }
+
+
+    public void showNoFlashError() {
+        AlertDialog alert = new AlertDialog.Builder(this)
+                .create();
+        alert.setTitle("Oops!");
+        alert.setMessage("Flash not available in this device...");
+        alert.setButton(DialogInterface.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        alert.show();
+    }
+
+    public void changeLedStatus(){
+        this.setLedStatus(!this.getLedStatus());
+        switchFlashLight(this.getLedStatus());
+        toggleButton.setChecked(this.getLedStatus());
+    }
+
+    public void switchFlashLight(boolean status) {
+        try {
+            mCameraManager.setTorchMode(mCameraId, status);
+            Log.d("SENSOR", "Status do led .." + String.valueOf(status));
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
 }
